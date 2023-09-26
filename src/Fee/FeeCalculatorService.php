3f539d43382831bdba771c5484e26358b5cdafe9
Bartłomiej Kołcz <bkolcz@bartisoft.com>
2023-09-26 19:43:39 +0200

    phpunit and tests
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      new file:   .env.test
      modified:   .gitignore
      new file:   bin/phpunit
      modified:   composer.json
      modified:   composer.lock
      modified:   config/bundles.php
      new file:   phpunit.xml.dist
      modified:   src/Fee/FeeCalculatorService.php
      modified:   symfony.lock
      new file:   tests/FeeCalculatorServiceTest.php
      new file:   tests/bootstrap.php

--- src/Fee/FeeCalculatorService.php
+++ src/Fee/FeeCalculatorService.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace App\Fee;
+
 use PragmaGoTech\Interview\Fee\FeeCalculator;
 use PragmaGoTech\Interview\Model\LoanProposal;
 
@@ -10,37 +12,39 @@ class FeeCalculatorService implements FeeCalculator
 
     public function calculate(LoanProposal $application): float
     {
-        return match ($application) {
-            12 === $application->term() => $this->oneYearLoan($application->amount()),
-            24 === $application->term() => $this->twoYearLoan($application->amount()),
+        return match ($application->term()) {
+            12 => $this->oneYearLoan($application->amount()),
+            24 => $this->twoYearLoan($application->amount()),
             default => -1.0
         };
     }
 
     private function oneYearLoan(float $amount): float
     {
-        return match ($amount) {
-            1000 <= $amount && 2000 < $amount => $this->normalizeFee($amount, 0.05),
-            2000 <= $amount && 3000 < $amount => $this->normalizeFee($amount, 0.045),
-            3000 <= $amount && 4000 < $amount => $this->normalizeFee($amount, 0.03),
-            4000 <= $amount && 20000 < $amount => $this->normalizeFee($amount, 0.02875),
+        return match (true) {
+            1000 <= $amount && 2000 > $amount => $this->normalizeFee($amount, 0.05),
+            2000 <= $amount && 3000 > $amount => $this->normalizeFee($amount, 0.045),
+            3000 <= $amount && 4000 > $amount => $this->normalizeFee($amount, 0.03),
+            4000 <= $amount && 5000 > $amount => $this->normalizeFee($amount, 0.02875),
+            5000 <= $amount && 20000 >= $amount => $this->normalizeFee($amount, 0.02),
             default => -1.0
         };
     }
     private function twoYearLoan(float $amount): float
     {
-        return match ($amount) {
-            1000 <= $amount && 2000 < $amount => $this->normalizeFee($amount, 0.05),
-            2000 <= $amount && 20000 < $amount => $this->normalizeFee($amount, 0.04),
+        return match (true) {
+            1000 <= $amount && 2000 > $amount => $this->normalizeFee($amount, 0.07),
+            2000 <= $amount && 3000 > $amount => $this->normalizeFee($amount, 0.05),
+            2000 <= $amount && 20000 >= $amount => $this->normalizeFee($amount, 0.04),
             default => -1.0
         };
     }
 
     private function normalizeFee(float $amount, float $interestRate): float
     {
-        $fee = $amount * $interestRate;
-        $total = $amount + $fee;
-        $reminder = $total % 5;
+        $fee = (float) $amount * (float) $interestRate;
+        $total = (float) $amount + (float) $fee;
+        $reminder = (float) $total % 5.0;
         return round($fee + $reminder, 2);
     }
 }
---
+++ tests/FeeCalculatorServiceTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Tests;
+
+use App\Fee\FeeCalculatorService;
+use PHPUnit\Framework\TestCase;
+use PragmaGoTech\Interview\Model\LoanProposal;
+
+class FeeCalculatorServiceTest extends TestCase
+{
+    /**
+     * @dataProvider oneYearProvider
+     */
+    public function testOneYear(float $amount, float $fee): void
+    {
+        $calculator = new FeeCalculatorService();
+        $proposal = new LoanProposal(12, (float) $amount);
+        $this->assertEquals((float) $fee, $calculator->calculate($proposal));
+    }
+
+    /**
+     * @dataProvider twoYearProvider
+     */
+    public function testTwoYear(float $amount, float $fee): void
+    {
+        $calculator = new FeeCalculatorService();
+        $proposal = new LoanProposal(24, (float)$amount);
+        $this->assertEquals((float) $fee, $calculator->calculate($proposal));
+    }
+
+    public function oneYearProvider()
+    {
+        return array(
+            array(1000, 50),
+            array(2000, 90),
+            array(3000, 90),
+            array(4000, 115),
+            array(5000, 100),
+            array(6000, 120),
+            array(7000, 140),
+            array(8000, 160),
+            array(9000, 180),
+            array(10000, 200),
+            array(11000, 220),
+            array(12000, 240),
+            array(13000, 260),
+            array(14000, 280),
+            array(15000, 300),
+            array(16000, 320),
+            array(17000, 340),
+            array(18000, 360),
+            array(19000, 380),
+            array(20000, 400),
+        );
+    }
+
+    /**
+     * Summary of twoYearProvider
+     * @return array
+     */
+    public function twoYearProvider()
+    {
+        return array(
+            array(1000, 70),
+            array(2000, 100),
+            array(3000, 120),
+            array(4000, 160),
+            array(5000, 200),
+            array(6000, 240),
+            array(7000, 280),
+            array(8000, 320),
+            array(9000, 360),
+            array(10000, 400),
+            array(11000, 440),
+            array(12000, 480),
+            array(13000, 520),
+            array(14000, 560),
+            array(15000, 600),
+            array(16000, 640),
+            array(17000, 680),
+            array(18000, 720),
+            array(19000, 760),
+            array(20000, 800),
+        );
+    }
+}
